81c51a16d01715e42967cb1486382eee63df0f8d
Jisan-Hasan <jisan.jh5@gmail.com>
2022-11-08 23:50:30 +0600

    mongodb connect with env variable

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+require('dotenv').config();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lxf9vua.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.get("/", (req, res) => {
     res.send("Server is responding");
